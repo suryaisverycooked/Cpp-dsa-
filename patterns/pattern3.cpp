3eca5b75485d347eee378b03fbe7052c9a6e6162
Surya <suryaparkerstarc@gmail.com>
2026-02-09 13:21:26 +0530

    added more sub patterns in pattern3

--- patterns/pattern3.cpp
+++ patterns/pattern3.cpp
@@ -19,3 +19,46 @@ int main()
         c=c+1;
     }
 }
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter Value of N: ";
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        char c ='A';
+        while(j<=n){
+            cout<<c<<" ";
+            j=j+1;
+            c=c+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    char c ='A';
+    cout<<"Enter Value of N: ";
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<c<<" ";
+            j=j+1;
+            c=c+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+}
+    diff pattern 
+    */
