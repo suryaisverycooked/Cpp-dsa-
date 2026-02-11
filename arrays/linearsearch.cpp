2cb3c82d75af673735d9fa86a1377bc7cedcf0e3
Surya <suryaparkerstarc@gmail.com>
2026-02-11 12:29:17 +0530

    changes

--- arrays/linearsearch.cpp
+++ arrays/linearsearch.cpp
@@ -1,3 +1,6 @@
+/* just use a key value and same loop and check if the incoming value is the key or not till the end of array. set a bool value
+found to be false, asuume there is no key present initially then in the loop if the condition that the element equals the key
+is true then modify value of found to true and break and then print found or not*/
 #include<iostream>
 using namespace std;
 
